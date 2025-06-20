1a7ee6bdec50420e42fe069ff68956feb464f119
Egon Rijpkema <git@egonrijpkema.nl>
2025-06-20 12:01:39 +0200

    My local setup. Probably will add more...

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<F5>",
+  'i<C-r>=strftime("## %a %d %b %Y %H:%M:%S %Z")<CR><Esc> /F5',
+  { noremap = true, silent = true, desc = "Insert timestamp with F5" }
+)
+
+vim.keymap.set("c", "w!!", "w !sudo tee % >/dev/null", { noremap = true, silent = true, desc = "Save as sudo" })
+
+vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "Git blame" })
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,48 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- If you'd rather extend the default config, use the code below instead:
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "tsx",
+        "typescript",
+        "typescript",
+        "vim",
+        "yaml",
+      })
+    end,
+  },
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+  {
+    "jabirali/vim-tmux-yank",
+  },
+}
